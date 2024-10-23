8819dbf38b465d638b0fb8b0e83a853e99c68d90
Julian Trujillo <jatmolina15@gmail.com>
2024-10-23 12:28:04 -0500

    Styling as masonry layout

--- src/pages/projects/project/index.tsx
+++ src/pages/projects/project/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from 'flowbite-react'
+import { FaEdit, FaPlus } from 'react-icons/fa'
 import MainCard from '@/components/cards/MainCard'
 import CommandCard from '@/modules/projects/components/CommandCard'
 
@@ -80,16 +82,25 @@ const ProjectPage = () => {
   return (
       <div className="px-4">
         <MainCard>
-          <h2 className="mb-3">
-            Grupo N1 SAS
-          </h2>
-          <p className="font-medium">
-            Lista de comandos utilizados en todos los proyectos que contiene n1, desde backend hasta frontend.
-          </p>
+          <div className="w-full flex items-center justify-between">
+            <div>
+              <h2 className="mb-3">
+                Grupo N1 SAS
+              </h2>
+              <p className="font-medium">
+                Lista de comandos utilizados en todos los proyectos que contiene n1, desde backend hasta frontend.
+              </p>
+            </div>
+            <Button color="purple" size="sm">
+              <FaPlus className="me-2"/>
+              Agregar grupo
+            </Button>
+          </div>
           <ul className="mt-8 list-none xl:columns-2 space-y-4 gap-4">
             {
               groups.map(group => (
-                  <li key={group.id} className="w-full bg-white/60 border rounded-2xl p-8 break-inside-avoid">
+                  <li key={group.id}
+                      className="relative w-full bg-white/60 border rounded-2xl p-8 group break-inside-avoid">
                     <h4>{group.title}</h4>
                     <div className="px-4 mt-8">
                       {
@@ -106,6 +117,12 @@ const ProjectPage = () => {
                             </div>
                         ))
                       }
+                    </div>
+                    <div
+                        className="absolute top-2 flex right-2 items-center gap-1 opacity-0 group-hover:opacity-100 transition duration-300">
+                      <button className="text-purple-800 p-2 rounded-full">
+                        <FaEdit/>
+                      </button>
                     </div>
                   </li>
               ))
