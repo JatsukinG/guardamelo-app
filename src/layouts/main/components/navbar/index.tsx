c8919749126740914ab894c1efbbe9b55d5219f6
jbayron09 <j.bayron.b@gmail.com>
2024-11-21 14:03:05 -0500

    implemented logic me and logoutn in navbar and sidebar

--- src/layouts/main/components/navbar/index.tsx
+++ src/layouts/main/components/navbar/index.tsx
@@ -1,4 +1,5 @@
 import { Avatar, CustomFlowbiteTheme, Dropdown, Navbar, TextInput } from 'flowbite-react'
+import { useAuth, useMe } from '@auth/hooks'
 
 //TODO extend color "transparent" for this in flowbite config
 const customTheme: CustomFlowbiteTheme['textInput'] = {
@@ -12,6 +13,17 @@ const customTheme: CustomFlowbiteTheme['textInput'] = {
 }
 
 const MainNavbar = () => {
+  const me = useMe()
+  const { logout } = useAuth()
+
+  const getPlaceholderInitials = (): string => {
+    const firstInitial = me?.firstName?.[0] ?? me?.lastName?.[0] ?? me?.email?.[0] ?? ''
+    const secondInitial = me?.firstName && me?.lastName ? me.lastName[0] :
+        me?.firstName?.[1] ?? me?.lastName?.[1] ?? me?.email?.[1] ?? ''
+
+    return (firstInitial + secondInitial).toUpperCase()
+  }
+
   return (
       <Navbar fluid rounded>
         <div className="w-full flex items-center justify-between">
@@ -26,22 +38,25 @@ const MainNavbar = () => {
               inline
               label={
                 <Avatar
+                    placeholderInitials={getPlaceholderInitials()}
                     rounded
-                    alt="User settings"
-                    placeholderInitials="AN"
-                    // img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                 />
               }
           >
             <Dropdown.Header>
-              <span className="block text-sm">Bonnie Green</span>
-              <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+              {
+                  me?.firstName && me?.lastName &&
+                  <span className="block truncate text-sm">{`${me.firstName} ${me.lastName}`}</span>
+              }
+              <span className="block truncate text-sm font-medium">{me?.email}</span>
             </Dropdown.Header>
             <Dropdown.Item>Dashboard</Dropdown.Item>
             <Dropdown.Item>Settings</Dropdown.Item>
             <Dropdown.Item>Earnings</Dropdown.Item>
             <Dropdown.Divider/>
-            <Dropdown.Item>Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={logout}>
+              <p className="ml-2">Cerrar sesión</p>
+            </Dropdown.Item>
           </Dropdown>
         </div>
       </Navbar>
